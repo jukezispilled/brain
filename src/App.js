f4e67c60d630d61933773beb13c0246ba90b20eb
Jack Anderson <whotfisjax@gmail.com>
2024-08-15 12:13:07 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <img src="dog.png" className='absolute right-0 bottom-0 w-[60%] md:w-[45%]'></img>
       <div className="absolute top-5 left-5 flex flex-col items-center z-10">
         <div className="flex flex-row">
-          <a href="https://x.com/" className="p-2 hover:scale-110 transition ease-in-out duration-200">
+          <a href="https://x.com/pvptoken" className="p-2 hover:scale-110 transition ease-in-out duration-200">
             <img src={Xlogo} alt="Xlogo" className="w-12 h-12 rounded-lg" />
           </a>
-          <a href="https://www.t.me/" className="p-2 hover:scale-110 transition ease-in-out duration-200">
+          <a href="https://www.t.me/solpvptoken" className="p-2 hover:scale-110 transition ease-in-out duration-200">
             <img src={TG} alt="Tg logo" className="w-12 h-12" />
           </a>
         </div>
